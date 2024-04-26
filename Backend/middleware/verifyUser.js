734bea12043f8c26b304e7af3f1d300f363f63ce
Aman Singh <singhaman38809@gmail.com>
2024-04-26 11:22:38 +0530

    complete user update functionality

--- Backend/middleware/verifyUser.js
+++ Backend/middleware/verifyUser.js
@@ -4,7 +4,7 @@ import { errorHandler } from "../middleware/error.js";
 export const  verifyUser = (req, res, next)=>{
     const token = req.cookies.access_token;
 
-    if (!token) return next(errorHandler(401,'Unaithorized'));
+    if (!token) return next(errorHandler(401,'Unauthorized'));
 
     jwt.verify(token, process.env.JWT_SECRET , (err, user)=>{
         if(err) return next(errorHandler(403, 'Forbidden'));
--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express' ;
-import { test, updateUser } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js'
 import { verifyUser } from '../middleware/verifyUser.js';
 
 const router = express.Router();
