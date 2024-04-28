d057eded9ab4c74c58ce8b6a5ab4b744d402589e
Aman Singh <singhaman38809@gmail.com>
2024-04-28 07:14:41 +0530

    create listingItem component and show listings on search

--- Frontend/src/Pages/Search.jsx
+++ Frontend/src/Pages/Search.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import ListingItem from "../components/ListingItem";
+import ListingItem from "../Components/ListingItem";
 
 export default function Search() {
   const navigate = useNavigate();
@@ -243,11 +243,11 @@ export default function Search() {
             </p>
           )}
 
-          {/* {!loading &&
+          {!loading &&
             listings &&
             listings.map((listing) => (
-            //   <ListingItem key={listing._id} listing={listing} />
-            ))} */}
+              <ListingItem key={listing._id} listing={listing} />
+            ))}
 
           {showMore && (
             <button
