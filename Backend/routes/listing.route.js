5d0fceb3a22d7421d6e7b8391e6de83afdcc1555
Aman Singh <singhaman38809@gmail.com>
2024-04-28 06:18:11 +0530

    serach api route comlete

--- Backend/routes/listing.route.js
+++ Backend/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express' ;
-import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing, getListings } from '../controllers/listing.controller.js';
 import { verifyUser } from '../middleware/verifyUser.js';
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post('/create',verifyUser , createListing);
 router.delete('/delete/:id',verifyUser , deleteListing);
 router.post('/update/:id',verifyUser , updateListing);
 router.get('/getListing/:id', getListing);
+router.get('/get', getListings);
 
 
 export default router 
