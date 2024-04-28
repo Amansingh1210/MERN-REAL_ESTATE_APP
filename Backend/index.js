1aa7a1f77349362846f8e4b25d6021a225a92aa5
Aman Singh <singhaman38809@gmail.com>
2024-04-28 17:59:03 +0530

    chages

--- Backend/index.js
+++ Backend/index.js
@@ -34,10 +34,10 @@ app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/Frontend/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'Frontend', 'dist', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
